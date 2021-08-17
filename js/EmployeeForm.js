323194ac8e666df51dc3989a6f6c5a82a6383ddc
TheJayesh99 <jayeshm.official@gmail.com>
2021-08-17 16:49:02 +0530

    Added : Method to remove employee

--- js/EmployeeForm.js
+++ js/EmployeeForm.js
@@ -64,12 +64,38 @@ function save(event) {
   event.stopPropagation()
   try {
     setEmployeePayrollObject()
-    createAndUpdateStorage()
-    resetForm()
-    window.location.replace(site_properties.home_page)
+    if (site_properties.use_local_storage.match("true")) {
+      createAndUpdateStorage()
+      resetForm()
+      window.location.replace(site_properties.home_page)
+    } else {
+      createAndUpdateEmployeeInServer()
+    }
   } catch (error) {
     alert(error);
   }
+}
+
+function createAndUpdateEmployeeInServer() {
+  let postUrl = site_properties.server_url
+  let methodCall = "POST"
+  if(isUpdate){
+    methodCall = "PUT"
+    postUrl = postUrl + employeePayrollObj.id.toString()
+  }
+  makePromiseCall(methodCall, postUrl, true, employeePayrollObj)
+  .then(
+    (responseText) =>
+    {
+      resetForm()
+      window.location.replace(site_properties.home_page)
+            }
+        )
+        .catch(
+          (error) =>{
+            throw error
+          }
+        );
 }
 
 function setEmployeePayrollObject() {
