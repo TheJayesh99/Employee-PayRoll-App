9166da7779b23ec3b67332a67088ecf971bebb68
TheJayesh99 <jayeshm.official@gmail.com>
2021-08-17 17:11:23 +0530

    Added : Method to remove data from server

--- js/site_properties.js
+++ js/site_properties.js
@@ -2,5 +2,5 @@ let site_properties = {
     use_local_storage:"false",
     home_page: "../pages/EmployeeHomePage.html",
     add_emp_page: "../pages/EmployeeForm.html",
-    server_url: " http://localhost:3000/EmployeePayrollDB"
+    server_url: " http://localhost:3000/EmployeePayrollDB/"
 }
